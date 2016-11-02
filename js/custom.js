8831d5cb7a3bb2922c0a482aa826fafe3e324deb
dadonimous <juric.dragan@gmail.com>
2016-11-02 13:02:22 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+$(function() {
+	$('.container').pIlluminate({ angleBadgesVisible: false, boxRadialGradientColorStop2: 'rgb(0, 81, 196)' });
+	alert('Testing css box-shadow, linear-gradient, radial-gradient and transition.');
+});
