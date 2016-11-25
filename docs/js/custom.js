a63c184851d097bbffbf9ee5036a66c6c56f07b5
dadonimous <juric.dragan@gmail.com>
2016-11-25 15:09:40 +0100

    Update custom.js

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,4 +1,4 @@
 $(function() {
-	$('.container').pIlluminate({ angleBadgesVisible: false, boxRadialGradientColorStop2: 'rgb(0, 81, 196)' });
+	$('.container').pIlluminate({ angleBadgesVisible: false, boxRadialGradientColorStop2: 'rgb(0, 73, 175)' });
 	$('body').on('mousemove', function(e) { $(this).trigger('illuminate.lightmove', [{ left: e.pageX, top: e.pageY }]); });
 });
